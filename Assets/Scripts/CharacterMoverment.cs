7feaff6034d57c23c2f3b684a8565d490169b710
DESKTOP-ETKC6SQ\truon <truong270901@gmail.com>
2023-12-13 01:15:58 +0700

    first commit

---
+++ Assets/Scripts/CharacterMoverment.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoverment : MonoBehaviour
+{
+    [Header("Referent")]
+    [SerializeField] private Rigidbody2D rigidbody2d;
+    [Header("Properties")]
+    [SerializeField] private float speedRun;
+    [SerializeField] private float speedJump;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+
+
+#endif
+
+
+    }
+
+
+    private void MoveLeft()
+    {
+
+    }
+
+    
+
+}
